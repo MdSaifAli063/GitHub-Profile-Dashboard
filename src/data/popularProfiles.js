73f368684ec6b9b3b502419c983411d030ef2253
MdSaifAli063 <mdsaifali6303@gmail.com>
2026-06-05 22:47:43 +0530

    ad

---
+++ src/data/popularProfiles.js
@@ -0,0 +1,18 @@
+export const popularProfiles = [
+  'torvalds',
+  'gaearon',
+  'sindresorhus',
+  'yyx990803',
+  'tj',
+  'addyosmani',
+  'kentcdodds',
+  'octocat',
+];
+
+export const leaderboardProfiles = [
+  'torvalds',
+  'sindresorhus',
+  'gaearon',
+  'yyx990803',
+  'addyosmani',
+];
